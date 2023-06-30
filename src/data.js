af6296717e5527af1276174e2239ab42c9c8b683
pratikshabaghel <pratikshasingh2604@gmail.com>
2023-06-30 11:25:50 +0530

    Create index.html

---
+++ src/data.js
@@ -0,0 +1,29 @@
+let shopItemsData = [{
+    id: "jgjhgjh",
+    name: "Party Gown",
+    price: "100",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-4.jpeg"
+},
+{
+    id: "mbnmb",
+    name: "Evening Gown",
+    price: "500",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-1.webp"
+},
+{
+    id: "assdse",
+    name: "Casual Gown",
+    price: "300",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-3.jpeg"
+},
+{
+    id: "wewerrt",
+    name: "Party Gown",
+    price: "600",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "images/img-2.webp",
+},
+];
